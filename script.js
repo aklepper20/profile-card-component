5a98e6d14341d4ccccbb1c50ed2fb48fada4b21e
Aly Klepper <alyklepper1@gmail.com>
2021-11-11 13:30:21 -0800

    adds steve profile picture

--- script.js
+++ script.js
@@ -22,11 +22,20 @@ let dog = {
 // document.getElementsByClassName('profile')[0].src = dog.profileImg;
 
 document.getElementById('name').innerHTML = dog.name;
-
+document.getElementsByClassName('age')[0].innerHTML = dog.age;
 document.getElementsByTagName('p')[0].innerHTML = dog.location;
 
+document.getElementsByClassName('profile-image')[0].style.backgroundImage = 'none';
+document.getElementsByClassName('profile-image')[0].style.backgroundImage = dog.bannerImg;
+
+document.getElementsByClassName('profile-pic')[0].style.backgroundImage = `url(${dog.profileImg})`
+// document.getElementsByClassName('profile-pic')[0].style.backgroundImage = 'none';
+
+
+//change background img to none so color can show
+document.body.style.backgroundColor = dog.backgroundColor;
+document.body.style.backgroundImage = 'none';
 
 document.getElementsByClassName('footer-number')[0].innerHTML = dog.stats.followers;
 document.getElementsByClassName('footer-number')[1].innerHTML = dog.stats.likes;
 document.getElementsByClassName('footer-number')[2].innerHTML = dog.stats.posts;
-document.getElementsByClassName('age')[0].innerHTML = dog.age;
