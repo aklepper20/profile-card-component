821c168fd34f747081a06ebbc5e84fd31cef1c89
Aly Klepper <alyklepper1@gmail.com>
2021-11-11 13:32:20 -0800

    completes comments

--- script.js
+++ script.js
@@ -14,28 +14,23 @@ let dog = {
     }
 };
 
-// How do I change the text color of the name to green?
-// How do I access the name?
-// How do I change the style?
-// How do I change the value of the style?
-// console.log(document.getElementById('name').style.color = "yellow")
-// document.getElementsByClassName('profile')[0].src = dog.profileImg;
-
+//change steves basic info
 document.getElementById('name').innerHTML = dog.name;
 document.getElementsByClassName('age')[0].innerHTML = dog.age;
 document.getElementsByTagName('p')[0].innerHTML = dog.location;
 
+//change steves banner background
 document.getElementsByClassName('profile-image')[0].style.backgroundImage = 'none';
 document.getElementsByClassName('profile-image')[0].style.backgroundImage = dog.bannerImg;
 
-document.getElementsByClassName('profile-pic')[0].style.backgroundImage = `url(${dog.profileImg})`
-// document.getElementsByClassName('profile-pic')[0].style.backgroundImage = 'none';
-
+//change profile picture to steve pic
+document.getElementsByClassName('profile-pic')[0].style.backgroundImage = `url(${dog.profileImg})`;
 
 //change background img to none so color can show
 document.body.style.backgroundColor = dog.backgroundColor;
 document.body.style.backgroundImage = 'none';
 
+//change stats to steves stats
 document.getElementsByClassName('footer-number')[0].innerHTML = dog.stats.followers;
 document.getElementsByClassName('footer-number')[1].innerHTML = dog.stats.likes;
 document.getElementsByClassName('footer-number')[2].innerHTML = dog.stats.posts;
