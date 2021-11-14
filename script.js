5c3bea0be0312f917ff2b8d1601231f984cc521b
Aly Klepper <alyklepper1@gmail.com>
2021-11-13 19:35:43 -0800

    adds functions to cat and dog to change cards

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log(document.body.innerHTML)
 
 let dog = {
     name: "Steve",
@@ -14,23 +13,92 @@ let dog = {
     }
 };
 
-//change steves basic info
-document.getElementById('name').innerHTML = dog.name;
-document.getElementsByClassName('age')[0].innerHTML = dog.age;
-document.getElementsByTagName('p')[0].innerHTML = dog.location;
 
-//change steves banner background
-document.getElementsByClassName('profile-image')[0].style.backgroundImage = 'none';
-document.getElementsByClassName('profile-image')[0].style.backgroundImage = dog.bannerImg;
+let cat = {
+    name: "Mochi",
+    age: 1,
+    location: "Philippines",
+    profileImg: "https://images.unsplash.com/photo-1621780030440-71c9ace78fa3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzF8fG9yYW5nZSUyMGNhdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+    bannerImg: "url('https://www.teahub.io/photos/full/159-1595602_chevron-desktop-wallpapers-hd-blue-striped-wallpaper-hd.jpg')",
+    backgroundColor: "#FFD580",
+    stats: {
+        followers: "4.5M",
+        likes: "12M",
+        posts: "8K"
+    },
 
-//change profile picture to steve pic
-document.getElementsByClassName('profile-pic')[0].style.backgroundImage = `url(${dog.profileImg})`;
+    speak() {
+        return "meow meow"
+    }
+}
 
-//change background img to none so color can show
-document.body.style.backgroundColor = dog.backgroundColor;
-document.body.style.backgroundImage = 'none';
+catProfile(cat);
 
-//change stats to steves stats
-document.getElementsByClassName('footer-number')[0].innerHTML = dog.stats.followers;
-document.getElementsByClassName('footer-number')[1].innerHTML = dog.stats.likes;
-document.getElementsByClassName('footer-number')[2].innerHTML = dog.stats.posts;
+function dogProfile(dog) {
+    document.getElementById('name').innerHTML = dog.name;
+    document.getElementsByClassName('age')[0].innerHTML = dog.age;
+    document.getElementsByTagName('p')[0].innerHTML = dog.location;
+    document.getElementsByClassName('profile-image')[0].style.backgroundImage = 'none';
+    document.getElementsByClassName('profile-image')[0].style.backgroundImage = dog.bannerImg;
+    document.getElementsByClassName('profile-pic')[0].style.backgroundImage = `url(${dog.profileImg})`;
+    document.body.style.backgroundColor = dog.backgroundColor;
+    document.body.style.backgroundImage = 'none';
+    document.getElementsByClassName('footer-number')[0].innerHTML = dog.stats.followers;
+    document.getElementsByClassName('footer-number')[1].innerHTML = dog.stats.likes;
+    document.getElementsByClassName('footer-number')[2].innerHTML = dog.stats.posts;
+}
+
+function catProfile(cat) {
+    document.getElementById('name').innerHTML = cat.name;
+    document.getElementsByClassName('age')[0].innerHTML = cat.age;
+    document.getElementsByTagName('p')[0].innerHTML = cat.location;
+    document.getElementsByClassName('profile-image')[0].style.backgroundImage = 'none';
+    document.getElementsByClassName('profile-image')[0].style.backgroundImage = cat.bannerImg;
+    document.getElementsByClassName('profile-pic')[0].style.backgroundImage = `url(${cat.profileImg})`;
+    document.body.style.backgroundColor = cat.backgroundColor;
+    document.body.style.backgroundImage = 'none';
+    document.getElementsByClassName('footer-number')[0].innerHTML = cat.stats.followers;
+    document.getElementsByClassName('footer-number')[1].innerHTML = cat.stats.likes;
+    document.getElementsByClassName('footer-number')[2].innerHTML = cat.stats.posts;
+}
+
+let container = document.body;
+let switchBtn = document.createElement("button");
+switchBtn.innerHTML = 'SWITCH PROFILES!';
+container.appendChild(switchBtn);
+switchBtn.style.margin = '10px 50px';
+switchBtn.style.padding = '8px';
+switchBtn.style.borderRadius = '6px';
+switchBtn.style.border = 'none';
+switchBtn.style.backgroundColor = '#cbb2fe';
+container.style.display = 'flex';
+container.style.flexDirection = 'column';
+
+switchBtn.addEventListener('click', function() {
+    document.getElementById('name').innerHTML == cat.name ? dogProfile(dog) : catProfile(cat);
+});
+
+// switchBtn.addEventListener('click', function() {
+//     if (document.getElementById('name').innerHTML == cat.name) {
+//         dogProfile(dog);
+//     } else {
+//         catProfile(cat);
+//     }
+// });
+
+// function styleBtn() {
+//   console.log('hi')
+//   btn.className == 'btn' ? btn.className = 'toggle-red' : btn.className = 'btn';
+// }
+
+// textChange.addEventListener('click', function() {
+//     if (textChange.style.color == 'black') {
+//         textChange.style.color = 'green'
+//     } else {
+//         textChange.style.color = 'black'
+//     }
+// })
+
+// textChange.addEventListener('click', function() {
+//     textChange.style.color == 'black' ? textChange.style.color = 'green' : textChange.style.color = 'black';
+// });
---
+++ switch.js
@@ -0,0 +1,22 @@
+// let container = document.body;
+// let switchBtn = document.createElement("button");
+// switchBtn.innerHTML = 'SWITCH PROFILES!';
+// container.appendChild(switchBtn);
+// switchBtn.style.margin = '10px 50px';
+// switchBtn.style.padding = '8px';
+// switchBtn.style.borderRadius = '6px';
+// switchBtn.style.border = 'none';
+// switchBtn.style.backgroundColor = '#cbb2fe';
+// container.style.display = 'flex';
+// container.style.flexDirection = 'column';
+
+
+// catProfile(cat)
+
+// switchBtn.addEventListener('click', function() {
+//   if (document.getElementById('name').innerHTML == cat.name) {
+//     dogProfile(dog);
+//   } else {
+//     catProfile(cat);
+//   }
+// });
